8acb0832e103441463f0a729e551296b5ed01cd1
Rahul B <93582933+RahulBisht001@users.noreply.github.com>
2023-02-24 10:42:13 +0530

    Create Longest Common Substring.java

---
+++ Dynammic Programming/Longest Common Substring.java
@@ -0,0 +1,80 @@
+
+/*
+ # Approach --
+  
+  Their are 2 case 
+  1. character at indices are same
+  2. character at indices are not same
+  
+  their are 3 possibilities in the 2 case
+  1. i is the part of lcs but not j
+  2. i is not the part of lcs but j
+  3. i and j both are not the part of lcs
+  
+  
+*/
+
+
+class Solution{
+    int longestCommonSubstr(String s1, String s2, int n, int m){
+       
+       return lcs(s1,s2,0,0,0);
+    }
+    
+    int lcs(String s1, String s2, int i, int j,int cnt){
+        
+        if(i >=s1.length() || j >= s2.length())
+           return cnt;
+           
+        int len = cnt;
+        
+        if(s1.charAt(i) == s2.charAt(j))
+            len =lcs(s1,s2,i+1,j+1,cnt+1);
+        
+        int diff1 = lcs(s1,s2,i+1,j,0);
+        int diff2 = lcs(s1,s2,i,j+1,0);
+        
+        return Math.max(len , Math.max(diff1,diff2));
+        
+    }
+}
+
+
+
+TLE
+// Memoization using HashMap
+class Solution{
+    int longestCommonSubstr(String s1, String s2, int n, int m){
+       
+      Map<String,Integer>dp = new HashMap<>();
+     
+      return lcs(s1,s2,0,0,0,dp);
+    }
+    
+    int lcs(String s1, String s2, int i, int j,
+        int cnt,Map<String,Integer>dp){
+        
+        if(i >=s1.length() || j >= s2.length())
+          return cnt;
+          
+        String Key = ""+ i+'#'+j+'@'+cnt;
+        if(dp.containsKey(Key)) 
+            return dp.get(Key);
+            
+        int len = cnt;
+        
+        if(s1.charAt(i) == s2.charAt(j))
+            len =lcs(s1,s2,i+1,j+1,cnt+1,dp);
+        
+        int diff1 = lcs(s1,s2,i+1,j,0,dp);
+        int diff2 = lcs(s1,s2,i,j+1,0,dp);
+        
+        int res = Math.max(len , Math.max(diff1,diff2));
+        
+        dp.put(Key,res);
+        return res;
+    }
+}
+
+
+
